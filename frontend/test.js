71158b939c98bea6a81622b687c53881bf316f45
Christopher Peters <chrissi.peters98@web.de>
2020-08-26 12:39:15 +0200

    hotfix

--- frontend/test.js
+++ frontend/test.js
@@ -23,7 +23,6 @@ function postUploadImageRequest(){
             }
         }
     }
-    request.setRequestHeader("Content-Type", "multipart/form-data")
     request.open("POST","/upload",true);
     request.send(formData);
 }
