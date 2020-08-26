bd200dff4300ac0f59777b0183483af8c7b8a673
Till <ti.fricke@ostfalia.de>
2020-08-26 15:29:05 +0200

    structure and slideshow

---
+++ frontend/js/test.js
@@ -0,0 +1,28 @@
+function createAjaxRequest(){
+    let request;
+    if(window.XMLHttpRequest){
+        request = new XMLHttpRequest();
+    }else{
+        request = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    return request;
+}
+
+
+function postUploadImageRequest(){
+    const inputFile = document.querySelector("#avatar").files[0];
+    const request = createAjaxRequest();
+    const formData = new FormData();
+    formData.append("imageFile", inputFile);
+    request.onreadystatechange = function(){
+        if(this.readyState === 4){
+            if(this.status === 200){
+
+            }else{
+                alert("failed: " +  this.status);
+            }
+        }
+    }
+    request.open("POST","/uploadImage",true);
+    request.send(formData);
+}
