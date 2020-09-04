e2662dcdb1dc6b180e66106297d0f2f76b32169b
Christopher Peters <chrissi.peters98@web.de>
2020-09-04 16:00:46 +0200

    added picture path

--- database.sql
+++ database.sql
@@ -5,14 +5,6 @@ CREATE TABLE users (
     Username varchar(255) NOT NULL UNIQUE,
     PasswordHash varchar(255) NOT NULL,
     Session_Id varchar(255) NOT NULL DEFAULT 0,
-    PRIMARY KEY(Id)
-);
-
-CREATE TABLE characters (
-    Users_Id int NOT NULL AUTO_INCREMENT,
-    Position_X int NOT NULL,
-    Position_Y int NOT NULL,
     Picture_Path varchar(255) NOT NULL DEFAULT '',
-    FOREIGN KEY (Users_Id) REFERENCES users(ID),
-    PRIMARY KEY (Users_Id)
+    PRIMARY KEY(Id)
 );
