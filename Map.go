43a2128db37a7da99d75a81e1ef75e226c06868e
Verbindolai <n.wieczorek@ostfalia.de>
2020-09-21 15:25:20 +0200

    - added function to create map from img prototype

--- Map.go
+++ Map.go
@@ -2,7 +2,12 @@ package main
 
 import (
 	"container/list"
+	"fmt"
+	"image"
+	"image/png"
+	"io"
 	"log"
+	"os"
 	"time"
 )
 
@@ -50,7 +55,7 @@ func NewMap(size int) Map {
 			m.Fields[i][j] = NewField()
 		}
 	}
-	FillTestMap(m)
+	CreateMapFromImage(m, "frontend/media/map.png")
 	return m
 }
 
@@ -366,6 +371,73 @@ func (w *Wall) getType() FieldObject {
 	}
 }
 
+func CreateMapFromImage(m Map, imagePfad string) {
+
+	image.RegisterFormat("png", "png", png.Decode, png.DecodeConfig)
+
+	file, err := os.Open(imagePfad)
+
+	if err != nil {
+		fmt.Println("Error: File could not be opened")
+		os.Exit(1)
+	}
+
+	defer file.Close()
+
+	pixels, err := getPixels(file)
+
+	if err != nil {
+		fmt.Println("Error: Image could not be decoded")
+		os.Exit(1)
+	}
+	wSolid := NewWall(false)
+	fmt.Println(pixels)
+
+	for i := 0; i < len(pixels); i++ {
+		for j := 0; j < len(pixels[i]); j++ {
+			if pixels[i][j].R == 0 && pixels[i][j].G == 0 && pixels[i][j].B == 0 && pixels[i][j].A == 255 {
+				m.Fields[i][j].addWall(wSolid)
+			}
+		}
+	}
+}
+
+// Get the bi-dimensional pixel array
+func getPixels(file io.Reader) ([][]Pixel, error) {
+	img, _, err := image.Decode(file)
+
+	if err != nil {
+		return nil, err
+	}
+
+	bounds := img.Bounds()
+	width, height := bounds.Max.X, bounds.Max.Y
+
+	var pixels [][]Pixel
+	for y := 0; y < height; y++ {
+		var row []Pixel
+		for x := 0; x < width; x++ {
+			row = append(row, rgbaToPixel(img.At(x, y).RGBA()))
+		}
+		pixels = append(pixels, row)
+	}
+
+	return pixels, nil
+}
+
+// img.At(x, y).RGBA() returns four uint32 values; we want a Pixel
+func rgbaToPixel(r uint32, g uint32, b uint32, a uint32) Pixel {
+	return Pixel{int(r / 257), int(g / 257), int(b / 257), int(a / 257)}
+}
+
+// Pixel struct example
+type Pixel struct {
+	R int
+	G int
+	B int
+	A int
+}
+
 func FillTestMap(m Map) {
 	//w0 := NewWall(true)
 	//w1 := NewWall(true)
@@ -419,8 +491,14 @@ func FillTestMap(m Map) {
 	i0 := NewItem(FieldObjectItemBoost)
 	i1 := NewItem(FieldObjectItemSlow)
 	i2 := NewItem(FieldObjectItemGhost)
-	p0 := NewPortal(newPosition(0, 1), newPosition(0, 18))
+	p0 := NewPortal(newPosition(9, 3), newPosition(8, 8))
+	p1 := NewPortal(newPosition(10, 3), newPosition(11, 8))
+	p2 := NewPortal(newPosition(8, 11), newPosition(9, 16))
+	p3 := NewPortal(newPosition(11, 11), newPosition(10, 16))
 	m.addPortal(&p0)
+	m.addPortal(&p1)
+	m.addPortal(&p2)
+	m.addPortal(&p3)
 
 	m.Fields[9][0].addWall(wSolid)
 	m.Fields[10][0].addWall(wSolid)
