9feace21685171c663aa6bef36d818bba8ac6a6f
Verbindolai <n.wieczorek@ostfalia.de>
2020-09-24 22:43:26 +0200

    - reorganized Go-Files - added Bomb.go, Bomberman.go and Movement.go

---
+++ Bomb.go
@@ -0,0 +1,144 @@
+package main
+
+import "time"
+
+type Bomb struct {
+	ID        uint64
+	Owner     *Bomberman
+	PositionX int
+	PositionY int
+	Time      int
+	Radius    int
+	state     int
+}
+
+func NewBomb(b *Bomberman) Bomb {
+	globalBombCount++
+	return Bomb{
+		ID:        globalBombCount,
+		Owner:     b,
+		PositionX: pixToArr(b.PositionX),
+		PositionY: pixToArr(b.PositionY),
+		Time:      b.bombTime,
+		Radius:    b.BombRadius,
+		state:     0,
+	}
+}
+
+func (b *Bomb) isAccessible() bool {
+	return false
+}
+func (b *Bomb) startEvent() {
+
+}
+func (b *Bomb) isDestructible() bool {
+	return false
+}
+func (b *Bomb) getType() FieldObject {
+	if b.state == 0 {
+		return FieldObjectBomb1
+	} else if b.state == 1 {
+		return FieldObjectBomb2
+	} else {
+		return FieldObjectBomb
+	}
+
+}
+
+func (f *Field) explosion() bool {
+	killAllPlayersOnField(f.Player)
+	for i := 0; i < 2; i++ {
+		if f.Contains[i] != nil {
+			if f.Contains[i].isDestructible() {
+				f.Contains[i] = nil
+
+			} else {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func (b *Bomb) startBomb() {
+	//Change to Loop
+	time.Sleep((time.Duration(b.Time) / bombStates) * time.Second)
+	b.state++
+	BuildAbstractGameMap()
+	time.Sleep((time.Duration(b.Time) / bombStates) * time.Second)
+	b.state++
+	BuildAbstractGameMap()
+
+	time.Sleep((time.Duration(b.Time) / bombStates) * time.Second)
+	b.state = 0
+
+	e := newExplosion()
+	x := b.PositionX
+	y := b.PositionY
+	xPosHitSolidWall, xNegHitSolidWall, yPosHitSolidWall, yNegHitSolidWall := false, false, false, false
+	GameMap.Fields[x][y].explosion()
+	e.ExpFields = append(e.ExpFields, newPosition(x, y))
+	for i := 1; i < b.Radius; i++ {
+		xPos := x + i
+		xNeg := x - i
+		yPos := y + i
+		yNeg := y - i
+		if xPos < len(GameMap.Fields) {
+			if !xPosHitSolidWall {
+				xPosHitSolidWall = GameMap.Fields[xPos][y].explosion()
+				if !xPosHitSolidWall {
+					e.ExpFields = append(e.ExpFields, newPosition(xPos, y))
+					GameMap.Fields[xPos][y].addExplosion(&e)
+				}
+			}
+		}
+		if xNeg >= 0 {
+			if !xNegHitSolidWall {
+				xNegHitSolidWall = GameMap.Fields[xNeg][y].explosion()
+				if !xNegHitSolidWall {
+					e.ExpFields = append(e.ExpFields, newPosition(xNeg, y))
+					GameMap.Fields[xNeg][y].addExplosion(&e)
+				}
+			}
+		}
+		if yPos < len(GameMap.Fields[x]) {
+			if !yPosHitSolidWall {
+				yPosHitSolidWall = GameMap.Fields[x][yPos].explosion()
+				if !yPosHitSolidWall {
+					e.ExpFields = append(e.ExpFields, newPosition(x, yPos))
+					GameMap.Fields[x][yPos].addExplosion(&e)
+				}
+			}
+		}
+		if yNeg >= 0 {
+			if !yNegHitSolidWall {
+				yNegHitSolidWall = GameMap.Fields[x][yNeg].explosion()
+				if !yNegHitSolidWall {
+					e.ExpFields = append(e.ExpFields, newPosition(x, yNeg))
+					GameMap.Fields[x][yNeg].addExplosion(&e)
+				}
+			}
+		}
+	}
+	if GameMap.Fields[x][y].Contains[0] == b {
+		GameMap.Fields[x][y].Contains[0] = nil
+	} else if GameMap.Fields[x][y].Contains[1] == b {
+		GameMap.Fields[x][y].Contains[1] = nil
+	}
+	GameMap.Fields[x][y].addExplosion(&e)
+	BuildAbstractGameMap()
+	time.Sleep(900 * time.Millisecond)
+	for i := 0; i < len(e.ExpFields); i++ {
+		if GameMap.Fields[e.ExpFields[i].x][e.ExpFields[i].y].Contains[0] != nil {
+			if GameMap.Fields[e.ExpFields[i].x][e.ExpFields[i].y].Contains[0].getType() == 9 {
+				GameMap.Fields[e.ExpFields[i].x][e.ExpFields[i].y].Contains[0] = nil
+			}
+		}
+		if GameMap.Fields[e.ExpFields[i].x][e.ExpFields[i].y].Contains[1] != nil {
+			if GameMap.Fields[e.ExpFields[i].x][e.ExpFields[i].y].Contains[1].getType() == 9 {
+				GameMap.Fields[e.ExpFields[i].x][e.ExpFields[i].y].Contains[1] = nil
+			}
+		}
+	}
+	BuildAbstractGameMap()
+}
--- Map.go
+++ Map.go
@@ -8,16 +8,12 @@ import (
 	"io"
 	"log"
 	"os"
-	"time"
 )
 
 type ItemType int
 type FieldObject int
 
-// -1 doesnt work
 var globalBombCount uint64 = 0
-
-//var globalTestMap Map = NewMap(10)
 
 const bombStates = 3
 
@@ -109,152 +105,11 @@ func (f *Field) addExplosion(e *Explosion) {
 	}
 }
 
-func (f *Field) explosion() bool {
-	killAllPlayersOnField(f.Player)
-	for i := 0; i < 2; i++ {
-		if f.Contains[i] != nil {
-			if f.Contains[i].isDestructible() {
-				f.Contains[i] = nil
-
-			} else {
-				return true
-			}
-		}
-	}
-	return false
-}
-
 type FieldType interface {
 	isAccessible() bool
 	startEvent()
 	isDestructible() bool
 	getType() FieldObject
-}
-
-type Bomb struct {
-	ID        uint64
-	Owner     *Bomberman
-	PositionX int
-	PositionY int
-	Time      int
-	Radius    int
-	state     int
-}
-
-func NewBomb(b *Bomberman) Bomb {
-	globalBombCount++
-	return Bomb{
-		ID:        globalBombCount,
-		Owner:     b,
-		PositionX: pixToArr(b.PositionX),
-		PositionY: pixToArr(b.PositionY),
-		Time:      b.bombTime,
-		Radius:    b.BombRadius,
-		state:     0,
-	}
-}
-
-func (b *Bomb) isAccessible() bool {
-	return false
-}
-func (b *Bomb) startEvent() {
-
-}
-func (b *Bomb) isDestructible() bool {
-	return false
-}
-func (b *Bomb) getType() FieldObject {
-	if b.state == 0 {
-		return FieldObjectBomb1
-	} else if b.state == 1 {
-		return FieldObjectBomb2
-	} else {
-		return FieldObjectBomb
-	}
-
-}
-
-func (b *Bomb) startBomb() {
-	//Change to Loop
-	time.Sleep((time.Duration(b.Time) / bombStates) * time.Second)
-	b.state++
-	BuildAbstractGameMap()
-	time.Sleep((time.Duration(b.Time) / bombStates) * time.Second)
-	b.state++
-	BuildAbstractGameMap()
-
-	time.Sleep((time.Duration(b.Time) / bombStates) * time.Second)
-	b.state = 0
-
-	e := newExplosion()
-	x := b.PositionX
-	y := b.PositionY
-	xPosHitSolidWall, xNegHitSolidWall, yPosHitSolidWall, yNegHitSolidWall := false, false, false, false
-	GameMap.Fields[x][y].explosion()
-	e.ExpFields = append(e.ExpFields, newPosition(x, y))
-	for i := 1; i < b.Radius; i++ {
-		xPos := x + i
-		xNeg := x - i
-		yPos := y + i
-		yNeg := y - i
-		if xPos < len(GameMap.Fields) {
-			if !xPosHitSolidWall {
-				xPosHitSolidWall = GameMap.Fields[xPos][y].explosion()
-				if !xPosHitSolidWall {
-					e.ExpFields = append(e.ExpFields, newPosition(xPos, y))
-					GameMap.Fields[xPos][y].addExplosion(&e)
-				}
-			}
-		}
-		if xNeg >= 0 {
-			if !xNegHitSolidWall {
-				xNegHitSolidWall = GameMap.Fields[xNeg][y].explosion()
-				if !xNegHitSolidWall {
-					e.ExpFields = append(e.ExpFields, newPosition(xNeg, y))
-					GameMap.Fields[xNeg][y].addExplosion(&e)
-				}
-			}
-		}
-		if yPos < len(GameMap.Fields[x]) {
-			if !yPosHitSolidWall {
-				yPosHitSolidWall = GameMap.Fields[x][yPos].explosion()
-				if !yPosHitSolidWall {
-					e.ExpFields = append(e.ExpFields, newPosition(x, yPos))
-					GameMap.Fields[x][yPos].addExplosion(&e)
-				}
-			}
-		}
-		if yNeg >= 0 {
-			if !yNegHitSolidWall {
-				yNegHitSolidWall = GameMap.Fields[x][yNeg].explosion()
-				if !yNegHitSolidWall {
-					e.ExpFields = append(e.ExpFields, newPosition(x, yNeg))
-					GameMap.Fields[x][yNeg].addExplosion(&e)
-				}
-			}
-		}
-	}
-	if GameMap.Fields[x][y].Contains[0] == b {
-		GameMap.Fields[x][y].Contains[0] = nil
-	} else if GameMap.Fields[x][y].Contains[1] == b {
-		GameMap.Fields[x][y].Contains[1] = nil
-	}
-	GameMap.Fields[x][y].addExplosion(&e)
-	BuildAbstractGameMap()
-	time.Sleep(900 * time.Millisecond)
-	for i := 0; i < len(e.ExpFields); i++ {
-		if GameMap.Fields[e.ExpFields[i].x][e.ExpFields[i].y].Contains[0] != nil {
-			if GameMap.Fields[e.ExpFields[i].x][e.ExpFields[i].y].Contains[0].getType() == 9 {
-				GameMap.Fields[e.ExpFields[i].x][e.ExpFields[i].y].Contains[0] = nil
-			}
-		}
-		if GameMap.Fields[e.ExpFields[i].x][e.ExpFields[i].y].Contains[1] != nil {
-			if GameMap.Fields[e.ExpFields[i].x][e.ExpFields[i].y].Contains[1].getType() == 9 {
-				GameMap.Fields[e.ExpFields[i].x][e.ExpFields[i].y].Contains[1] = nil
-			}
-		}
-	}
-	BuildAbstractGameMap()
 }
 
 type Explosion struct {
@@ -356,6 +211,27 @@ func (w *Wall) getType() FieldObject {
 		return FieldObjectWeakWall
 	} else {
 		return FieldObjectSolidWall
+	}
+}
+
+func BuildAbstractGameMap() {
+	//Create map to send
+	abstractGameMap = make([][][]FieldObject, len(GameMap.Fields))
+	//testToSend := make([][]int, len(GameMap.Fields))
+	for i, _ := range GameMap.Fields {
+		abstractGameMap[i] = make([][]FieldObject, len(GameMap.Fields[i]))
+		//testToSend[i] = make([]int, len(GameMap.Fields[i]))
+		for j, _ := range GameMap.Fields[i] {
+			abstractGameMap[i][j] = make([]FieldObject, len(GameMap.Fields[i][j].Contains))
+			if GameMap.Fields[i][j].Player.Front() != nil {
+				//testToSend[i][j] = 1
+			}
+			for k, _ := range GameMap.Fields[i][j].Contains {
+				if GameMap.Fields[i][j].Contains[k] != nil {
+					abstractGameMap[i][j][k] = GameMap.Fields[i][j].Contains[k].getType()
+				}
+			}
+		}
 	}
 }
 
